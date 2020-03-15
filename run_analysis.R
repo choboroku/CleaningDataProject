fbd7bfb4e62f8ee05a54cfb40a2f613ce43d8642
arlamng74 <alfonso@falcon-computing.com>
2020-03-14 23:17:36 -0700

    Re-submit assignment

--- run_analysis.R
+++ run_analysis.R
@@ -5,27 +5,35 @@
 rm(list=ls())
 
 ####################################################################
+# Download dataset:
+
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+filename <- "UCI_HAR_Dataset.zip"  # String obtained from above 
+download.file(fileURL, filename, method="curl")
+unzip(filename)
+
+####################################################################
 # 1.-Merges the training and the test sets to create one data set.
 ####################################################################
 
 # Read Features:
-features<-read.table("features.txt") 
+features<-read.table("UCI HAR Dataset/features.txt") 
 
 # For Train Dataset:
-subject.train <- read.table("train/subject_train.txt", header=FALSE)                                                                  
+subject.train <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)                                                                  
 names(subject.train)<-"Subject"                                                                                                       
-activities.train <- read.table("train/y_train.txt")                                                                                   
+activities.train <- read.table("UCI HAR Dataset/train/y_train.txt")                                                                                   
 names(activities.train) <- "Activity"                   
-train <-read.table("train/X_train.txt")
+train <-read.table("UCI HAR Dataset/train/X_train.txt")
 names(train) <- features$V2
 train.dataset <- data.frame(subject.train, activities.train, train)
 
 # For Test Dataset:                                                                                                                  
-subject.test <- read.table("test/subject_test.txt", header=FALSE) 
+subject.test <- read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE) 
 names(subject.test)<-"Subject"
-activities.test <- read.table("test/y_test.txt") 
+activities.test <- read.table("UCI HAR Dataset/test/y_test.txt") 
 names(activities.test) <- "Activity"                                                                                                 
-test <-read.table("test/X_test.txt")
+test <-read.table("UCI HAR Dataset/test/X_test.txt")
 names(test) <- features$V2 
 test.dataset <- data.frame(subject.test, activities.test, test)      
 
@@ -65,9 +73,42 @@ names(tidy.dataset.reduced) <- gsub("std.[.]", "std", names(tidy.dataset.reduced
 # with the average of each variable for each activity and each subject
 ####################################################################
 
-select <- c(1,2, grep("mean", names(tidy.dataset.reduced)))
-tidy.dataset.reduced2 <- tidy.dataset.reduced[select]
+subjects.index <- levels(factor(tidy.dataset.reduced$Subject))
+activities.index <- levels(factor(tidy.dataset.reduced$Activity))
+
+# Redefine Variables Names to make them easy to read: 
+new.names <- paste("average", names(tidy.dataset.reduced)[3:ncol(tidy.dataset.reduced)], sep="-")
+new.names <- gsub("-t", "-Time", new.names)
+new.names <- gsub("Acc", "Accelerometer", new.names)
+new.names <- gsub("Gyro", "Gyroscope", new.names)
+new.names <- gsub("Mag", "Magnitude", new.names)
+new.names <- gsub("-f", "-Frequency", new.names)
+new.names <- gsub("BodyBody", "Body", new.names)
+
+# Defining an empty dataframe:
+tidy.data = tidy.dataset.reduced[FALSE,]
+
+for (i in c(subjects.index)){
+   for (j in c(activities.index)){
+
+        # Getting the Subject and Activity:
+        info <- tidy.dataset.reduced[tidy.dataset.reduced$Subject==i 
+				  & tidy.dataset.reduced$Activity==j,][1,1:2]
+
+        # Compute Average for each variable:
+        data<-apply(tidy.dataset.reduced[tidy.dataset.reduced$Subject==i
+            & tidy.dataset.reduced$Activity==j, 3:ncol(tidy.dataset.reduced)], 2, mean, na.rm=TRUE)    
+
+	# Putting all together:
+        tidy.data <- rbind(tidy.data, data.frame(info, t(as.data.frame(data))))
+
+   }
+}
+
+# Update the column names:
+new.names <-c("Subject", "Activity", new.names)
+names(tidy.data) <- new.names
 
 # Saving the data into a file
-write.table(tidy.dataset.reduced2, "tidy.mean.only.txt", row.name=FALSE)
+write.table(tidy.data, "tidy.mean.only.txt", row.name=FALSE)
 
